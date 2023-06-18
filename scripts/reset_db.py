c17a7f8d1108beb54b79916dcdbdce66ed355c37
Mani <mani.mozaffar@gmail.com>
2023-06-18 22:18:23 +0300

    added: alembic and db utilities

---
+++ scripts/reset_db.py
@@ -0,0 +1,14 @@
+import asyncio
+
+from src.core.database import SQL_DB
+from src.models import MarketPlace, Profile, User
+
+__all__ = ["Profile", "MarketPlace", "User"]
+
+
+async def up_database():
+    await SQL_DB.drop_tables()
+    await SQL_DB.create_tables()
+    print("DONE!")
+
+asyncio.run(up_database())
